359692ea02416d9ee89469336d75cff8c0e0b777
Veeraraj Veeramani <veeraraj.v@gmail.com>
2022-07-17 23:26:51 +0200

    Crated base
    added new xCode project to the repository

---
+++ FourSquare/FourSquare/FourSquareApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourSquareApp.swift
+//  FourSquare
+//
+//  Created by Veera on 17/07/22.
+//
+
+import SwiftUI
+
+@main
+struct FourSquareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
